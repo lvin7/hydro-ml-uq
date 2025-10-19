b53fea05681a7ee57a0bebb99852f5a94e557875
eln7 <lukavinokic@yahoo.com>
2025-10-19 09:16:54 +0200

    metrics.py added, fill later

---
+++ metrics.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
+
+# -----------------------
+# Metrics helper functions
+# -----------------------
+
+def plot_loss(history):
+    pass
+
+def scatter_plot(y_pred, y_true):
+    pass
+
+def ts_plot(y_pred, y_true):
+    pass
+
+def metrics_table(y_pred, y_true):
+    pass
+
+# copy from google colab notebooks
